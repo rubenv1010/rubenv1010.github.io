b3569110ade0596044e697084880c09893a848c4
Ruben Vermeulen <r0844726@student.vives.be>
2023-03-03 11:57:27 +0100

    Labo3

---
+++ Labo3Java/oplossing colorpicker(3)/oplossing colorpicker/scripts/code.js
@@ -0,0 +1,23 @@
+const initialize = () => {
+	let sliders = document.getElementsByClassName("slider");
+	for (let i = 0; i < sliders.length; i++) {
+		// we moeten zowel op het input als het change event reageren,
+		// zie http://stackoverflow.com/questions/18544890
+		sliders[i].addEventListener("change", update);
+		sliders[i].addEventListener("input", update);
+	}
+	update();
+};
+
+const update = () => {
+	let red=document.getElementById("sldRed").value; //input always value
+	let green=document.getElementById("sldGreen").value;
+	let blue=document.getElementById("sldBlue").value;
+	document.getElementById("lblRed").innerHTML=red;
+	document.getElementById("lblGreen").innerHTML=green;// html-element innerHTML
+	document.getElementById("lblBlue").innerHTML=blue;
+	let swatch=document.getElementById("swatch");
+	swatch.style.backgroundColor="rgb("+red+","+green+","+blue+")";
+};
+
+window.addEventListener("load", initialize);
